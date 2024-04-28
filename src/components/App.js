d1444edc3d938b57c9d0b6c5a7ebbd2213e1c56e
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-04-28 11:30:59 +0000

    asdas

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
-
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 const App = () => {
+  const[inputValue, setInputValue]=useState("");
+  const [suggestedArray, setSuggestedArray]=useState([...fruits]);
+
+  console.log(suggestedArray);
+  useEffect(()=>{
+    setTimeout(()=>{
+    let array=fruits.filter(fruit=>fruit.includes(inputValue));
+    setSuggestedArray(array);
+    }, 500)
+  }, [inputValue])
   return (
     <div>
-        {/* Do not remove the main div */}
+       <h1>Search item</h1>
+       <input type="text" value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
+       <div>
+        <ul>
+          {
+            suggestedArray.map(fruit=><li key={fruit}>{fruit}</li>)
+          }
+        </ul>
+       </div>
     </div>
   )
 }
